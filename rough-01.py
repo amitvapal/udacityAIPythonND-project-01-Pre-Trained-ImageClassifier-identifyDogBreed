7b69faa0ea4b6e64c6ccfc2bbe5c37992395e935
Amitva Pal <amitvapal@amitvas-air.lan>
2020-08-01 13:28:16 -0400

    udacityAIPythonND-project-01-Pre-Trained-ImageClassifier-identifyDogBreed First commit

---
+++ rough-01.py
@@ -0,0 +1,59 @@
+
+
+
+
+from os import listdir
+
+image_dir = "/Users/amitvapal/Documents/Udacity-AI-Python/02-IntroductionToPyhton/project/AIPND-revision-master/intropyproject-classify-pet-images/pet_images"
+in_files = listdir(image_dir)
+
+
+
+for idx in range(0, len(in_files), 1):
+
+    
+    # Skips file if starts with . (like .DS_Store of Mac OSX) because it 
+    # isn't an pet image file
+    if in_files[idx][0] != ".":
+        
+        # Creates temporary label variable to hold pet label name extracted 
+        pet_label = in_files[idx].split("_")
+        #print(pet_label)
+
+        # TODO: 2a. BELOW REPLACE pass with CODE that will process each 
+        #          filename in the in_files list to extract the dog breed 
+        #          name from the filename. Recall that each filename can be
+        #          accessed by in_files[idx]. Be certain to place the 
+        #          extracted dog breed name in the variable pet_label 
+        #          that's created as an empty string ABOVE
+        if len(pet_label) == 2:
+            #print(pet_label[0])
+            pet_label_01 = pet_label[0]
+        elif len(pet_label) == 3:
+            #print(" ".join(pet_label[0:2]))
+            pet_label_01 = " ".join(pet_label[0:2])
+        elif len(pet_label) == 4:
+            print(" ".join(pet_label[0:3]))
+            pet_label_01 = " ".join(pet_label[0:3])
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
